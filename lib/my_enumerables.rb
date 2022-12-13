4c04aa4bdb92317a223fd0146c314a0583216f9f
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-12-13 10:48:53 +0100

    Create my_each_with_index method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,13 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    for i in 0...self.length do
+      j = self[i]
+      yield(j, i)
+    end
+    self
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
   def my_each
     for i in self do
